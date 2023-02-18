80c6e046bec75321cce19ddc2ed482fc5db36895
Valentine Anagbogu <104782642+valentineanagbogu@users.noreply.github.com>
2023-02-18 08:48:53 +0000

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,10 +1,15 @@
+// start
 job('NodeJS example') {
+    
+    // git repository which will be used in the job
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/valentineanagbogu/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
+    
+    // how often you want to build
     triggers {
         scm('H/5 * * * *')
     }
@@ -12,6 +17,8 @@ job('NodeJS example') {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
+    
+    //build steps
     steps {
         shell("npm install")
     }
